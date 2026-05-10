2df3b9b5193f82fa47a86c0a47d97ab58aa9404c
Inna Finchenko <innafinchenko@Innas-MacBook-Air.local>
2026-05-10 12:20:10 +0200

    Update homework files 1 and 2

--- goit-pycore-hw-03.py/goit-pycore-hw-03-1.py
+++ goit-pycore-hw-03.py/goit-pycore-hw-03-1.py
@@ -2,12 +2,10 @@ from datetime import datetime
 
 def get_days_from_today(date):
     try:
-         target_date = datetime.date(datetime.strptime(date, '%Y-%m-%d'))
+         target_date = datetime.strptime(date, '%Y-%m-%d').date()
          current_date = datetime.now().date()
          difference = current_date - target_date
          return difference.days
     except ValueError:
         return "Wrong format"
-    else:
-        return difference.days
 print(get_days_from_today('2026-08-10'))
--- goit-pycore-hw-03.py/goit-pycore-hw-03-2.py
+++ goit-pycore-hw-03.py/goit-pycore-hw-03-2.py
@@ -1,10 +1,7 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    min >= 1
-    max <=1000
-    min < quantity <= max
-    return sorted (random.sample(range(1,1000), 6))
+    return sorted (random.sample(range(min, max), quantity))
 
 lottery_numbers = get_numbers_ticket(1, 1000, 6)
 print("Ваші лотерейні числа:",lottery_numbers)
