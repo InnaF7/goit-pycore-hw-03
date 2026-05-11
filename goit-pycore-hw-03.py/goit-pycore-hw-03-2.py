c58b374ad98761f3942a81ce16b6d524a263803f
Inna Finchenko <innafinchenko@Innas-MacBook-Air.local>
2026-05-11 22:01:11 +0200

    Your message

--- goit-pycore-hw-03.py/goit-pycore-hw-03-2.py
+++ goit-pycore-hw-03.py/goit-pycore-hw-03-2.py
@@ -1,7 +1,9 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    return sorted (random.sample(range(min, max), quantity))
+    if min < 1 or max > 1000 or quantity < 1 or quantity > (max - min + 1):
+        return []
+    return sorted (random.sample(range(min, max + 1), quantity))
 
 lottery_numbers = get_numbers_ticket(1, 1000, 6)
 print("Ваші лотерейні числа:",lottery_numbers)
